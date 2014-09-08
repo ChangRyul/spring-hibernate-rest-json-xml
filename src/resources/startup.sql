0cce35ac8d7f996b00fd600544b5fad3cb4e1b0f
lesula <enrico.costanzi@gmail.com>
2014-09-08 13:30:22 +0000

    Sql startup script IGNORE already existing lines.

--- src/resources/startup.sql
+++ src/resources/startup.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-INSERT INTO `User` (`Id`, `FirstName`, `LastName`) VALUES
+INSERT IGNORE INTO `User` (`Id`, `FirstName`, `LastName`) VALUES
 (1, 'Saul', 'Goodman'),
 (2, 'Walter', 'White'),
 (3, 'Jessie', 'Pinkman'),
